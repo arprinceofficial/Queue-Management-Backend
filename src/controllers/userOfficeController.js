010f40bd0f972bda42a808d2f1d0bc653f3c7be9
arprinceofficial <ashiqurr04@gmail.com>
2024-08-08 21:52:36 +0600

    fix update user controller

--- src/controllers/userOfficeController.js
+++ src/controllers/userOfficeController.js
@@ -6,139 +6,6 @@ const { sign } = require("jsonwebtoken");
 const transporter = require('./emailController');
 
 module.exports = {
-    // Create a new user
-    async createUser(req, res) {
-        const {
-            first_name,
-            last_name,
-            gender,
-            email,
-            password,
-            mobile_number,
-            role_id,
-        } = req.body;
-
-        try {
-            // Check if user email exists
-            const userExists = await prisma.user.findFirst({ where: { email, role_id: 1 } });
-            if (userExists) {
-                return res.status(400).json({
-                    code: 400,
-                    status: "error",
-                    message: 'This email already exists'
-                });
-            }
-            // Check if user phone number exists
-            const phoneNumberExists = await prisma.user.findFirst({ where: { mobile_number, role_id: 1 } });
-            if (phoneNumberExists) {
-                return res.status(400).json({
-                    code: 400,
-                    status: "error",
-                    message: 'This phone number already exists'
-                });
-            }
-            // Check mobile number length
-            if (mobile_number.length !== 11) {
-                return res.status(400).json({
-                    code: 400,
-                    status: "error",
-                    message: 'Mobile number must be 11 digits'
-                });
-            }
-
-            // Hash the password
-            const salt = genSaltSync(10);
-            const hashedPassword = hashSync(password, salt);
-
-            // Create the user
-            const newUser = await prisma.user.create({
-                data: {
-                    first_name,
-                    last_name,
-                    gender,
-                    email,
-                    mobile_number,
-                    is_validated: 0,
-                    password: hashedPassword,
-                    created_at: new Date(),
-                    updated_at: new Date(),
-                    role: {
-                        connect: {
-                            id: parseInt(role_id),
-                        },
-                    },
-                },
-            });
-            res.status(201).json({
-                code: 201,
-                status: "success",
-                data: {
-                    // access_token: token,
-                    user: {
-                        id: newUser.id,
-                        first_name: newUser.first_name,
-                        last_name: newUser.last_name,
-                        gender: newUser.gender,
-                        email: newUser.email,
-                        mobile_number: newUser.mobile_number,
-                        role_id: newUser.role_id,
-                    }
-                }
-            });
-        } catch (error) {
-            res.status(500).json({
-                code: 500,
-                status: "error",
-                message: error.message
-            });
-        }
-    },
-    // Profile image upload
-    async uploadUserImage(req, res) {
-        const { id } = req.params;
-        const { imagepath } = req.params;
-        try {
-            const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
-            if (!user) {
-                return res.status(404).json({
-                    code: 404,
-                    status: "error",
-                    message: 'User not found'
-                });
-            }
-            if (req.file) {
-                const updatedUser = await prisma.user.update({
-                    where: { id: parseInt(id) },
-                    data: {
-                        [imagepath + '_image']: req.file.filename,
-                        updated_at: new Date(),
-                    },
-                });
-                res.status(200).json({
-                    code: 200,
-                    status: "success",
-                    message: `${imagepath} image uploaded successfully`,
-                    data: {
-                        id: updatedUser.id,
-                        first_name: updatedUser.first_name,
-                        last_name: updatedUser.last_name,
-                    }
-                });
-            } else {
-                res.status(400).json({
-                    code: 400,
-                    status: "error",
-                    message: `${imagepath} image is required`
-                });
-            }
-        } catch (error) {
-            res.status(500).json({
-                code: 500,
-                status: "error",
-                message: error.message
-            });
-        }
-    },
     // login 
     async login(req, res) {
         const { loginInput, password } = req.body;
@@ -258,6 +125,30 @@ module.exports = {
             });
         }
     },
+    // Current Login user
+    async currentUser(req, res) {
+        try {
+            if (req.auth_user) {
+                res.status(200).json({
+                    code: 200,
+                    status: "success",
+                    data: req.auth_user
+                });
+            } else {
+                res.status(401).json({
+                    code: 401,
+                    status: "error",
+                    message: 'Access denied! unauthorized user'
+                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                code: 500,
+                status: "error",
+                message: error.message
+            });
+        }
+    },
     // logout
     async logout(req, res) {
         const id = req.auth_user.user.id;
@@ -289,109 +180,12 @@ module.exports = {
         }
 
     },
-    // Current Login user
-    async currentUser(req, res) {
+    // Profile image upload
+    async uploadUserImage(req, res) {
+        const id = req.auth_user.user.id;
+        const { imagepath } = req.params;
         try {
-            if (req.auth_user) {
-                res.status(200).json({
-                    code: 200,
-                    status: "success",
-                    data: req.auth_user
-                });
-            } else {
-                res.status(401).json({
-                    code: 401,
-                    status: "error",
-                    message: 'Access denied! unauthorized user'
-                });
-            }
-        } catch (error) {
-            res.status(500).json({
-                code: 500,
-                status: "error",
-                message: error.message
-            });
-        }
-    },
-    // Get all users
-    async getAllUsers(req, res) {
-        try {
-            const users = await prisma.user.findMany();
-            res.status(200).json({
-                data: {
-                    code: 200,
-                    status: "success",
-                    data: users.map((user) => ({
-                        id: user.id,
-                        first_name: user.first_name,
-                        last_name: user.last_name,
-                        mobile_number: user.mobile_number,
-                        gender: user.gender,
-                        profile_image: user.profile_image ? `${req.protocol + '://' + req.get('host')}/profile_images/${user.profile_image}` : null,
-                        passport_image: user.passport_image ? `${req.protocol + '://' + req.get('host')}/passport_images/${user.passport_image}` : null,
-                        is_validated: user.is_validated,
-                        email: user.email,
-                        created_at: user.created_at,
-                        updated_at: user.updated_at,
-                    })),
-                }
-            });
-        } catch (error) {
-            res.status(500).json({
-                code: 500,
-                status: "error",
-                message: error.message
-            });
-        }
-    },
-    // Get a single user by ID
-    async getUserById(req, res) {
-        const { id } = req.params;
-        try {
-            const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
-            if (user) {
-                res.status(200).json({
-                    data: {
-                        code: 200,
-                        status: "success",
-                        data: {
-                            id: user.id,
-                            first_name: user.first_name,
-                            last_name: user.last_name,
-                            mobile_number: user.mobile_number,
-                            gender: user.gender,
-                            profile_image: user.profile_image ? `${req.protocol + '://' + req.get('host')}/profile_images/${user.profile_image}` : null,
-                            passport_image: user.passport_image ? `${req.protocol + '://' + req.get('host')}/passport_images/${user.passport_image}` : null,
-                            is_validated: user.is_validated,
-                            email: user.email,
-                            created_at: user.created_at,
-                            updated_at: user.updated_at,
-                        }
-                    }
-                });
-            } else {
-                res.status(404).json({
-                    code: 404,
-                    status: "error",
-                    message: 'User not found'
-                });
-            }
-        } catch (error) {
-            res.status(500).json({
-                code: 500,
-                status: "error",
-                message: error.message
-            });
-        }
-    },
-    // Update a user
-    async updateUser(req, res) {
-        const { id } = req.params;
-        const { first_name, last_name, gender, email, password, mobile_number } = req.body;
-        try {
-            // If user not exists
-            const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
-
+            const user = await prisma.user.findFirst({ where: { id: parseInt(id), role_id: 1 } });
             if (!user) {
                 return res.status(404).json({
                     code: 404,
@@ -399,78 +193,31 @@ module.exports = {
                     message: 'User not found'
                 });
             }
-
-            //  Check if user email exists
-            // const userExists = await prisma.user.findUnique({ where: { email } });
-            // if (userExists) {
-            //     return res.status(400).json({ error: 'This email already exists' });
-            // }
-
-            // Hash the password
-            const salt = genSaltSync(10);
-            const hashedPassword = hashSync(password, salt);
-
-            // Update the user
-            const updatedUser = await prisma.user.update({
-                where: { id: parseInt(id) },
-                data: {
-                    first_name,
-                    last_name,
-                    gender,
-                    email,
-                    password: hashedPassword,
-                    mobile_number,
-                    updated_at: new Date(),
-                },
-            });
-            res.status(200).json({
-                data: {
+            if (req.file) {
+                const updatedUser = await prisma.user.update({
+                    where: { id: parseInt(id) },
+                    data: {
+                        [imagepath + '_image']: req.file.filename,
+                        updated_at: new Date(),
+                    },
+                });
+                res.status(200).json({
                     code: 200,
                     status: "success",
-                    message: 'User updated successfully',
+                    message: `${imagepath} image uploaded successfully`,
                     data: {
                         id: updatedUser.id,
                         first_name: updatedUser.first_name,
                         last_name: updatedUser.last_name,
-                        gender: updatedUser.gender,
-                        email: updatedUser.email,
-                        mobile_number: updatedUser.mobile_number,
-                        created_at: updatedUser.created_at,
-                        updated_at: updatedUser.updated_at,
                     }
-                }
-            });
-
-        } catch (error) {
-            res.status(500).json({
-                code: 500,
-                status: "error",
-                message: error.message
-            });
-        }
-    },
-    // Delete a user
-    async deleteUser(req, res) {
-        const { id } = req.params;
-        try {
-            // If user not exists
-            const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
-            if (!user) {
-                return res.status(404).json({
-                    code: 404,
+                });
+            } else {
+                res.status(400).json({
+                    code: 400,
                     status: "error",
-                    message: 'User not found'
+                    message: `${imagepath} image is required`
                 });
             }
-
-            // Delete the user
-            await prisma.user.delete({ where: { id: parseInt(id) } });
-            res.status(200).json({
-                code: 200,
-                status: "success",
-                message: 'User deleted successfully'
-            });
-
         } catch (error) {
             res.status(500).json({
                 code: 500,
@@ -479,6 +226,7 @@ module.exports = {
             });
         }
     },
+
     // Otp request sent to email
     async otpRequestEmail(req, res) {
         const { email } = req.body;
--- src/routes/userOfficRoutes.js
+++ src/routes/userOfficRoutes.js
@@ -3,18 +3,11 @@ const { authMiddleware } = require('../middleware/authMiddleware');
 const userOfficeController = require('../controllers/userOfficeController');
 const imageController = require('../controllers/imageController');
 
-
-router.post('/user', userOfficeController.createUser);
 router.post('/login', userOfficeController.login);
-router.post('/logout', userOfficeController.logout);
 router.get('/current-user', authMiddleware, userOfficeController.currentUser);
-router.get('/user', authMiddleware, userOfficeController.getAllUsers);
-router.get('/user/:id', authMiddleware, userOfficeController.getUserById);
-router.put('/user/:id', authMiddleware, userOfficeController.updateUser);
-router.delete('/user/:id', authMiddleware, userOfficeController.deleteUser);
+router.get('/logout', authMiddleware, userOfficeController.logout);
+router.post('/:imagepath/upload', authMiddleware, imageController.singleImageUpload, userOfficeController.uploadUserImage);
 router.put('/otp-request', userOfficeController.otpRequestEmail);
 router.put('/otp-verify', userOfficeController.verifyOtp);
-router.post('/user/:imagepath/:id', authMiddleware, imageController.singleImageUpload, userOfficeController.uploadUserImage);
-router.get('/logout', authMiddleware, userOfficeController.logout);
 
 module.exports = router;
