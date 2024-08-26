e87fb0a15b9b09fc2a3cf8c07bf164a32bd34fee
arprinceofficial <ashiqurr04@gmail.com>
2024-08-26 14:18:52 +0600

    add total: country.length for counter api

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1715,6 +1715,7 @@ module.exports = {
                 return res.status(200).json({
                     code: 200,
                     status: true,
+                    total: country.length,
                     data: country,
                 });
             }
